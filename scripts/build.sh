f8eba3b65ae0df829c3f1c43213d7e040d06fbda
wokron <stringcatwok@gmail.com>
2023-11-12 14:24:10 +0800

    style: modify build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,12 +2,12 @@
 
 if [ "$1" = "clean" ]
 then
-    echo "clean build results"
+    echo "clean out directory"
     rm ./out/* 2>/dev/null
-    rmdir ./out 2>/dev/null
     exit 0
 fi
 
 echo "build client and server"
 go build -o $GONTP_OUT_DIR/gontp-client $GONTP_HOME/cmd/gontp-client
 go build -o $GONTP_OUT_DIR/gontp-server $GONTP_HOME/cmd/gontp-server
+echo "finish"
