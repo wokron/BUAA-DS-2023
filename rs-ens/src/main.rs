e2e85cb6c60f5a381d2489c689fff83b56d1e4aa
wokron <stringcatwok@gmail.com>
2023-11-25 22:48:03 +0800

    feat: simple consumer client example

--- rs-ens/src/main.rs
+++ rs-ens/src/main.rs
@@ -1,3 +1,32 @@
+use std::net::TcpStream;
+
+use enslib::{client::subscribe, conn::recv_ens_msg};
+
+mod enslib;
+
 fn main() {
-    println!("Hello, world!");
+    let mut stream = TcpStream::connect("localhost:4567").unwrap();
+
+    let topics = vec![
+        String::from("topic1"),
+        String::from("topic2"),
+        String::from("topic3"),
+    ];
+
+    for topic in topics {
+        if let Err(err) = subscribe(&mut stream, &topic) {
+            println!("Fail to subscribe topic \"{}\", {}", topic, err);
+        }
+    }
+
+    loop {
+        if let Some(msg) = recv_ens_msg(&mut stream) {
+            println!(
+                "Received Event, topic: \"{}\", message: \"{}\".",
+                msg.topic, msg.message
+            );
+        } else {
+            println!("error when receiving")
+        }
+    }
 }
